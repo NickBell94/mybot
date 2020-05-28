2a4d7d5b77bf12a1339912b8c9f72c8af2f5762e
NickBell94 <35149662+NickBell94@users.noreply.github.com>
2020-05-28 12:35:45 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const Telegraf = require('telegraf');
-const bot = new Telegraf('1292763139:AAHcUoyovekYdLjvP0ymCKSjze5WtgPvqtw');
-bot.start((ctx) => {
-    console.log('Id пользователя:', ctx.from.id);
-    return ctx.reply('Добро пожаловать!');
-  });
-  bot.on('message', (msg) => ctx.reply('why not?'));
-  bot.hears('hi', (ctx) => ctx.reply('Hey there'));
